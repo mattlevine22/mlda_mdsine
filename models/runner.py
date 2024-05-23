2cac9a68ad548f9e00aee9a2da76173cb9febdc2
mattlevine22 <mattlevine22@gmail.com>
2024-05-23 17:28:27 -0400

    manage devices in callback

--- models/runner.py
+++ models/runner.py
@@ -41,6 +41,7 @@ class PredictionCheckpoint(ModelCheckpoint):
 
         # Ensure model is in evaluation mode
         pl_module.eval()
+        device = pl_module.device
         stages = ["train", "val"]  # You can add 'test' if you run test after each epoch
 
         output = {}
@@ -65,7 +66,7 @@ class PredictionCheckpoint(ModelCheckpoint):
                         x_ref_mean,
                     ) = batch
                     y_pred, y_assim, x_pred, x_assim, cov, inv_cov = pl_module(
-                        y_obs, times, controls, x_ref_mean.requires_grad_(True)
+                        y_obs.to(device), times.to(device), controls.to(device), x_ref_mean.to(device).requires_grad_(True)
                     )
                     output[stage][ind] = {
                         "times": times.detach().cpu().numpy(),
